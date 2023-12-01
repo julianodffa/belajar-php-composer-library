1bbb642c31ce76b4534c9f1724d2d3561c45fe25
Juliano Daffa Adytia <julianodaffaadytia@gmail.com>
2023-12-01 13:55:28 +0700

    First Commit

---
+++ vendor/src/Greetings.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace JulianoDaffaAdytia\Belajar;
+
+class Greetings
+{
+    private string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function sayHello(string $name = "Guest"): string
+    {
+        return "Hello $name, My name is $name.";
+    }
+}
